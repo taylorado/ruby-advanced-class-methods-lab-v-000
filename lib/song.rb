4ae02b590a79876cfcbaa4a534b73ed2b70100a9
Taylor Donlan <taylordonlan@gmail.com>
2018-03-10 14:42:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,6 +27,12 @@ class Song
     song
   end
 
+  def self.new_by_name(string_name_of_the_song)
+    song = self.new
+    song.name = string_name_of_the_song
+    song
+  end
+
   def self.find_by_name(string_name_of_the_song)
     self.all.detect {|i| i.name == string_name_of_the_song}
   end
